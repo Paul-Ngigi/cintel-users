30750b9cc45af9190ce390a57d7b492d5f6a172f
Paul-Ngigi <paulkush7777@gmail.com>
2025-02-15 16:23:38 +0300

    update routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+  ],
 };
